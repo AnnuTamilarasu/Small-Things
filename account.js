ed007dd971433e43ab720780bb79f7b091b5cd41
AXkyJellyfish <kaiyanxiao3@gmail.com>
2025-11-29 16:44:13 -0500

    new

--- account.js
+++ account.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const { MongoClient } = require("mongodb");
+const { ObjectId } = require("mongodb");
 
 const app = express();
 app.use(cors());
@@ -30,7 +31,7 @@ startServer();
 
 app.post("/signup", async (req, res) => {
   try {
-    const { username, password, phone, id, mood, perfer, avatar } = req.body;
+    const { username, password, phone, mood, perfer, avatar } = req.body;
 
     if (!username || !password) return res.json({ success: false, message: "Missing username or password" });
 
@@ -38,7 +39,6 @@ app.post("/signup", async (req, res) => {
     if (existing) return res.json({ success: false, message: "User exists" });
 
     await usersCollection.insertOne({ username, password,phone,
-                        id,
                         mood,
                         perfer,
                         avatar, });
@@ -63,8 +63,9 @@ app.post("/login", async (req, res) => {
 
     res.json({ success: true,
         user:{
-        id: user.id,
+        id: user._id.toString(),
         username: user.username,
+        password:user.password,
         phone: user.phone,
         mood: user.mood,
         perfer: user.perfer,
@@ -75,5 +76,25 @@ app.post("/login", async (req, res) => {
   } catch (err) {
     console.error(err);
     res.json({ success: false, message: "Server error" });
+  }
+});
+app.post("/profile",async(req,res)=>{
+  try{
+    let {id,avatar} = req.body;
+    if (!id) return res.json({ success: false, message: "No id provided" });
+    id=id.trim();
+    const user = await usersCollection.findOne({ _id: new ObjectId(id) });
+if (!user) return res.json({ success: false, message: "User not found" });
+    const result=await usersCollection.updateOne(
+      {_id:new ObjectId(id)},
+      {
+        $set:{avatar}
+      }
+    );
+
+        res.json({ success: true });
+  }catch{
+    console.error(err);
+        res.json({ success: false, message: "Database error" });
   }
 });
